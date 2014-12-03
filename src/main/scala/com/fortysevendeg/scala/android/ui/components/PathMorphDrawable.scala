2d1169a199537a87fd7a5a4c7155f43a29685320
franciscodr <francisco.d@47deg.com>
2014-12-03 10:47:22 +0100

    SA-11 PathMorphDrawable Improvements: Changed UI PathMorphingActivity and added some Tweaks

--- src/main/scala/com/fortysevendeg/scala/android/ui/components/PathMorphDrawable.scala
+++ src/main/scala/com/fortysevendeg/scala/android/ui/components/PathMorphDrawable.scala
@@ -7,10 +7,12 @@ import android.graphics._
 import android.graphics.drawable.{Animatable, Drawable}
 import android.view.animation.DecelerateInterpolator
 import android.widget.ImageView
-import macroid.FullDsl._
 import macroid.{AppContext, Tweak}
 
-class PathMorphDrawable(implicit appContext: AppContext) extends Drawable with Animatable with PathMorphDrawableTypes {
+class PathMorphDrawable(val defaultIcon: Int = TypeIcons.NOICON, val defaultStroke: Int = 3, val defaultColor: Int = Color.BLACK)(implicit appContext: AppContext)
+    extends Drawable
+    with Animatable
+    with PathMorphDrawableTypes {
 
   implicit var size: Option[Dim] = None
 
@@ -59,12 +61,14 @@ class PathMorphDrawable(implicit appContext: AppContext) extends Drawable with A
     new Segment().fromRatios(0.288f, 0.288f, 0.712f, 0.712f)
   )
 
+  val noIcon = List.empty
+
   val iconPaint: Paint = {
     val paint = new Paint
     paint.setAntiAlias(true)
     paint.setStyle(Style.STROKE)
-    paint.setStrokeWidth(3 dp)
-    paint.setColor(Color.BLACK)
+    paint.setStrokeWidth(defaultStroke)
+    paint.setColor(defaultColor)
     paint
   }
 
@@ -79,10 +83,10 @@ class PathMorphDrawable(implicit appContext: AppContext) extends Drawable with A
   override def onBoundsChange(bounds: Rect): Unit = {
     super.onBoundsChange(bounds)
     size = Some(new Dim(bounds.width(), bounds.height()))
+    setTypeIcon(defaultIcon)
   }
 
   override def draw(canvas: Canvas): Unit = {
-    canvas.drawColor(Color.GRAY)
     if (running) {
       transformIcon.map(drawIcon(canvas, _))
     } else {
@@ -116,8 +120,19 @@ class PathMorphDrawable(implicit appContext: AppContext) extends Drawable with A
     }
   }
 
+  def setColor(color: Int): Unit = {
+    iconPaint.setColor(color)
+    invalidateSelf()
+  }
+
+  def setColorResource(color: Int): Unit = {
+    iconPaint.setColor(appContext.get.getResources.getColor(color))
+    invalidateSelf()
+  }
+
   def setStroke(stroke: Float) = {
     iconPaint.setStrokeWidth(stroke)
+    invalidateSelf()
   }
 
   def setTransformIcon(icon: Icon) = {
@@ -130,47 +145,35 @@ class PathMorphDrawable(implicit appContext: AppContext) extends Drawable with A
     invalidateSelf()
   }
 
-  def setTypeIcon(icon: Int) = {
-    if (icon == TypeIcons.BURGER) {
-      setIcon(burgerIcon)
-    } else if (icon == TypeIcons.BACK) {
-      setIcon(backIcon)
-    } else if (icon == TypeIcons.UP) {
-      setIcon(upIcon)
-    } else if (icon == TypeIcons.DOWN) {
-      setIcon(downIcon)
-    } else if (icon == TypeIcons.NEXT) {
-      setIcon(nextIcon)
-    } else if (icon == TypeIcons.CHECK) {
-      setIcon(checkIcon)
-    } else if (icon == TypeIcons.ADD) {
-      setIcon(addIcon)
-    } else if (icon == TypeIcons.CLOSE) {
-      setIcon(closeIcon)
-    }
-  }
-
   def setToIcon(icon: Icon) = {
     toIcon = Some(icon)
   }
 
+  def setTypeIcon(icon: Int) = {
+    icon match {
+      case TypeIcons.ADD => setIcon(addIcon)
+      case TypeIcons.BACK => setIcon(backIcon)
+      case TypeIcons.BURGER => setIcon(burgerIcon)
+      case TypeIcons.CHECK => setIcon(checkIcon)
+      case TypeIcons.CLOSE => setIcon(closeIcon)
+      case TypeIcons.DOWN => setIcon(downIcon)
+      case TypeIcons.NEXT => setIcon(nextIcon)
+      case TypeIcons.NOICON => setIcon(noIcon)
+      case TypeIcons.UP => setIcon(upIcon)
+    }
+  }
+
   def setToTypeIcon(icon: Int) = {
-    if (icon == TypeIcons.BURGER) {
-      setToIcon(burgerIcon)
-    } else if (icon == TypeIcons.BACK) {
-      setToIcon(backIcon)
-    } else if (icon == TypeIcons.UP) {
-      setToIcon(upIcon)
-    } else if (icon == TypeIcons.DOWN) {
-      setToIcon(downIcon)
-    } else if (icon == TypeIcons.NEXT) {
-      setToIcon(nextIcon)
-    } else if (icon == TypeIcons.CHECK) {
-      setToIcon(checkIcon)
-    } else if (icon == TypeIcons.ADD) {
-      setToIcon(addIcon)
-    } else if (icon == TypeIcons.CLOSE) {
-      setToIcon(closeIcon)
+    icon match {
+      case TypeIcons.ADD => setToIcon(addIcon)
+      case TypeIcons.BACK => setToIcon(backIcon)
+      case TypeIcons.BURGER => setToIcon(burgerIcon)
+      case TypeIcons.CHECK => setToIcon(checkIcon)
+      case TypeIcons.CLOSE => setToIcon(closeIcon)
+      case TypeIcons.DOWN => setToIcon(downIcon)
+      case TypeIcons.NEXT => setToIcon(nextIcon)
+      case TypeIcons.NOICON => setToIcon(noIcon)
+      case TypeIcons.UP => setToIcon(upIcon)
     }
   }
 
@@ -251,7 +254,6 @@ class PathMorphDrawable(implicit appContext: AppContext) extends Drawable with A
     else
       Point(from.x - coordX, from.y - coordY)
   }
-
 }
 
 trait PathMorphDrawableTypes {
@@ -259,14 +261,15 @@ trait PathMorphDrawableTypes {
 }
 
 object TypeIcons {
-  val BURGER = 0
-  val BACK = 1
-  val CHECK = 2
-  val ADD = 3
-  val UP = 4
-  val DOWN = 5
-  val NEXT = 6
-  val CLOSE = 7
+  val NOICON = 0
+  val BURGER = 1
+  val BACK = 2
+  val CHECK = 3
+  val ADD = 4
+  val UP = 5
+  val DOWN = 6
+  val NEXT = 7
+  val CLOSE = 8
 }
 
 case class Dim(wight: Int, height: Int)
@@ -290,20 +293,20 @@ case class Segment(
         val y2 = ratioY2 * value.height
         (x1, y1, x2, y2)
     }.getOrElse(0f, 0f, 0f, 0f, 0f)
-    new Segment(Point(x1, y1), Point(x2, y2))
+    Segment(Point(x1, y1), Point(x2, y2))
   }
-
 }
 
 object PathMorphDrawableTweaks {
   type W = ImageView
 
-  def changeIcon(icon: Int) = Tweak[W](_.getDrawable.asInstanceOf[PathMorphDrawable].setTypeIcon(icon))
-
-  def animIcon(icon: Int) = Tweak[W] {
+  def pmdAnimIcon(icon: Int) = Tweak[W] {
     view =>
       view.getDrawable.asInstanceOf[PathMorphDrawable].setToTypeIcon(icon)
       view.getDrawable.asInstanceOf[PathMorphDrawable].start
   }
-
+  def pmdChangeIcon(icon: Int) = Tweak[W](_.getDrawable.asInstanceOf[PathMorphDrawable].setTypeIcon(icon))
+  def pmdColor(color: Int) = Tweak[W](_.getDrawable.asInstanceOf[PathMorphDrawable].setColor(color))
+  def pmdColorResource(color: Int) = Tweak[W](_.getDrawable.asInstanceOf[PathMorphDrawable].setColorResource(color))
+  def pmdStroke(stroke: Float) = Tweak[W](_.getDrawable.asInstanceOf[PathMorphDrawable].setStroke(stroke))
 }
--- src/main/scala/com/fortysevendeg/scala/android/ui/pathmorphing/Layout.scala
+++ src/main/scala/com/fortysevendeg/scala/android/ui/pathmorphing/Layout.scala
@@ -1,71 +1,169 @@
 package com.fortysevendeg.scala.android.ui.pathmorphing
 
-import android.view.Gravity
+import android.graphics.Color
+import android.view.View
 import android.widget._
 import com.fortysevendeg.scala.android.R
-import com.fortysevendeg.scala.android.macroid.FrameLayoutTweaks._
-import com.fortysevendeg.scala.android.macroid.ImageViewTweaks._
-import com.fortysevendeg.scala.android.ui.commons.ToolbarLayout
-import Styles._
-import com.fortysevendeg.scala.android.ui.components.{TypeIcons, PathMorphDrawable}
-import macroid.{Ui, ActivityContext, AppContext}
-import macroid.FullDsl._
-import com.fortysevendeg.scala.android.macroid.ToolbarTweaks._
+import com.fortysevendeg.scala.android.macroid.ExtraSeekBarEvents.OnSeekBarChangeListenerHandler
+import com.fortysevendeg.scala.android.macroid.SeekBarTweaks._
 import com.fortysevendeg.scala.android.macroid.TextTweaks._
+import com.fortysevendeg.scala.android.macroid.ToolbarTweaks._
+import com.fortysevendeg.scala.android.macroid.ViewTweaks._
+import com.fortysevendeg.scala.android.ui.commons.ToolbarLayout
 import com.fortysevendeg.scala.android.ui.components.PathMorphDrawableTweaks._
+import com.fortysevendeg.scala.android.ui.components.{Dim, TypeIcons}
+import com.fortysevendeg.scala.android.ui.pathmorphing.Styles._
+import macroid.FullDsl._
+import macroid.{ActivityContext, AppContext, Transformer}
 
 trait Layout extends ToolbarLayout {
 
+  val sizeOptionList = List(Dim(48, 48), Dim(72, 72), Dim(96, 96), Dim(128, 128))
+
   var icon = slot[ImageView]
+  var sampleColor1Icon = slot[ImageView]
+  var sampleColor2Icon = slot[ImageView]
+  var sampleColor3Icon = slot[ImageView]
+  var sampleColor4Icon = slot[ImageView]
+  var iconSelectorGroup = slot[LinearLayout]
+  var colorSelectorGroup = slot[LinearLayout]
+  var strokeTitle = slot[TextView]
+  var sizeTitle = slot[TextView]
+  var strokeSelector = slot[SeekBar]
+  var sizeSelector = slot[SeekBar]
+
+  val deactivateImageViewWidgets = Transformer {
+    case i: ImageView => i <~ vActivated(false)
+  }
+
+  val setNoIconImageViewWidgets = Transformer {
+    case i: ImageView => i <~ pmdAnimIcon(TypeIcons.NOICON)
+  }
 
   def layout(implicit appContext: AppContext, context: ActivityContext) = {
     getUi(
       l[LinearLayout](
         toolBarLayout <~ tbTitle(R.string.title_path_morphing),
-        l[FrameLayout](
-          l[LinearLayout](
-            l[RadioGroup](
-              w[RadioButton] <~ tvText("Burger") + radioItemStyle <~ On.click {
-                icon <~ animIcon(TypeIcons.BURGER)
-              },
-              w[RadioButton] <~ tvText("Back") <~ radioItemStyle <~ On.click {
-                icon <~ animIcon(TypeIcons.BACK)
-              },
-              w[RadioButton] <~ tvText("Check") <~ radioItemStyle <~ On.click {
-                icon <~ animIcon(TypeIcons.CHECK)
-              },
-              w[RadioButton] <~ tvText("Add") <~ radioItemStyle <~ On.click {
-                icon <~ animIcon(TypeIcons.ADD)
-              },
-              w[RadioButton] <~ tvText("Up") <~ radioItemStyle <~ On.click {
-                icon <~ animIcon(TypeIcons.UP)
-              },
-              w[RadioButton] <~ tvText("Down") <~ radioItemStyle <~ On.click {
-                icon <~ animIcon(TypeIcons.DOWN)
-              },
-              w[RadioButton] <~ tvText("Next") <~ radioItemStyle <~ On.click {
-                icon <~ animIcon(TypeIcons.NEXT)
-              },
-              w[RadioButton] <~ tvText("Close") <~ radioItemStyle <~ On.click {
-                icon <~ animIcon(TypeIcons.CLOSE)
-              }
-            ) <~ contentItemsRadiosStyle,
-            l[RadioGroup](
-              w[RadioButton] <~ tvText("48x48") + radioItemStyle <~ On.click {
-                icon <~ flContentSize(48 dp, 48 dp) <~ flLayoutGravity(Gravity.CENTER) + ivSrc(new PathMorphDrawable())
-              },
-              w[RadioButton] <~ tvText("72x72") <~ radioItemStyle <~ On.click {
-                icon <~ flContentSize(72 dp, 72 dp) <~ flLayoutGravity(Gravity.CENTER) + ivSrc(new PathMorphDrawable())
-              },
-              w[RadioButton] <~ tvText("94x94") <~ radioItemStyle <~ On.click {
-                icon <~ flContentSize(94 dp, 94 dp) <~ flLayoutGravity(Gravity.CENTER) + ivSrc(new PathMorphDrawable())
-              },
-              w[RadioButton] <~ tvText("128x128") <~ radioItemStyle <~ On.click {
-                icon <~ flContentSize(128 dp, 128 dp) <~ flLayoutGravity(Gravity.CENTER) + ivSrc(new PathMorphDrawable())
-              }
-            ) <~ contentItemsRadiosStyle
-          ) <~ contentRadiosStyle,
-          w[ImageView] <~ drawableStyle <~ wire(icon)
+        l[LinearLayout](
+          w[ImageView] <~ drawableStyle(width = 48 dp, height = 48 dp, stroke = 3 dp) <~ wire(icon),
+          l[ScrollView](
+            l[LinearLayout](
+              w[TextView] <~ tvText(R.string.title_select_icon) <~ titleStyle,
+              l[LinearLayout](
+                l[LinearLayout](
+                  w[ImageView] <~ iconSelectorStyle(TypeIcons.BURGER, false) <~ FuncOn.click {
+                    (view: View) => {
+                      (iconSelectorGroup <~ deactivateImageViewWidgets) ~
+                          (icon <~ pmdAnimIcon(TypeIcons.BURGER)) ~
+                          (view <~ vActivated(true))
+                    }
+                  },
+                  w[ImageView] <~ iconSelectorStyle(TypeIcons.CHECK, false) <~ FuncOn.click {
+                    (view: View) => {
+                      (iconSelectorGroup <~ deactivateImageViewWidgets) ~
+                          (icon <~ pmdAnimIcon(TypeIcons.CHECK)) ~
+                          (view <~ vActivated(true))
+                    }
+                  },
+                  w[ImageView] <~ iconSelectorStyle(TypeIcons.ADD, false) <~ FuncOn.click {
+                    (view: View) => {
+                      (iconSelectorGroup <~ deactivateImageViewWidgets) ~
+                          (icon <~ pmdAnimIcon(TypeIcons.ADD)) ~
+                          (view <~ vActivated(true))
+                    }
+                  },
+                  w[ImageView] <~ iconSelectorStyle(TypeIcons.CLOSE, false) <~ FuncOn.click {
+                    (view: View) => {
+                      (iconSelectorGroup <~ deactivateImageViewWidgets) ~
+                          (icon <~ pmdAnimIcon(TypeIcons.CLOSE)) ~
+                          (view <~ vActivated(true))
+                    }
+                  }
+                ) <~ tableLayoutRowStyle,
+                l[LinearLayout](
+                  w[ImageView] <~ iconSelectorStyle(TypeIcons.UP, false) <~ FuncOn.click {
+                    (view: View) => {
+                      (iconSelectorGroup <~ deactivateImageViewWidgets) ~
+                          (icon <~ pmdAnimIcon(TypeIcons.UP)) ~
+                          (view <~ vActivated(true))
+                    }
+                  },
+                  w[ImageView] <~ iconSelectorStyle(TypeIcons.NEXT, false) <~ FuncOn.click {
+                    (view: View) => {
+                      (iconSelectorGroup <~ deactivateImageViewWidgets) ~
+                          (icon <~ pmdAnimIcon(TypeIcons.NEXT)) ~
+                          (view <~ vActivated(true))
+                    }
+                  },
+                  w[ImageView] <~ iconSelectorStyle(TypeIcons.DOWN, false) <~ FuncOn.click {
+                    (view: View) => {
+                      (iconSelectorGroup <~ deactivateImageViewWidgets) ~
+                          (icon <~ pmdAnimIcon(TypeIcons.DOWN)) ~
+                          (view <~ vActivated(true))
+                    }
+                  },
+                  w[ImageView] <~ iconSelectorStyle(TypeIcons.BACK, false) <~ FuncOn.click {
+                    (view: View) => {
+                      (iconSelectorGroup <~ deactivateImageViewWidgets) ~
+                          (icon <~ pmdAnimIcon(TypeIcons.BACK)) ~
+                          (view <~ vActivated(true))
+                    }
+                  }
+                ) <~ tableLayoutRowStyle
+              ) <~ tableLayoutStyle <~ wire(iconSelectorGroup),
+              w[TextView] <~ tvText(R.string.title_select_color) <~ titleStyle,
+              l[LinearLayout](
+                l[LinearLayout](
+                  w[ImageView] <~ colorSelectorStyle(true) <~ wire(sampleColor1Icon) <~ pmdColor(Color.WHITE) <~ vBackground(R.drawable.background_item_pathmorphsample_1) <~ On.click {
+                    (icon <~ pmdColorResource(R.color.pathMorphSample1)) ~
+                        (colorSelectorGroup <~ setNoIconImageViewWidgets) ~
+                        (sampleColor1Icon <~ pmdAnimIcon(TypeIcons.CHECK))
+                  },
+                  w[ImageView] <~ colorSelectorStyle(false) <~ wire(sampleColor2Icon) <~ vBackground(R.drawable.background_item_pathmorphsample_2) <~ On.click {
+                    (icon <~ pmdColorResource(R.color.pathMorphSample2)) ~
+                        (colorSelectorGroup <~ setNoIconImageViewWidgets) ~
+                        (sampleColor2Icon <~ pmdAnimIcon(TypeIcons.CHECK))
+                  },
+                  w[ImageView] <~ colorSelectorStyle(false) <~ wire(sampleColor3Icon) <~ vBackground(R.drawable.background_item_pathmorphsample_3) <~ On.click {
+                    (icon <~ pmdColorResource(R.color.pathMorphSample3)) ~
+                        (colorSelectorGroup <~ setNoIconImageViewWidgets) ~
+                        (sampleColor3Icon <~ pmdAnimIcon(TypeIcons.CHECK))
+                  },
+                  w[ImageView] <~ colorSelectorStyle(false) <~ wire(sampleColor4Icon) <~ pmdColor(Color.WHITE) <~ vBackground(R.drawable.background_item_pathmorphsample_4) <~ On.click {
+                    (icon <~ pmdColorResource(R.color.pathMorphSample4)) ~
+                        (colorSelectorGroup <~ setNoIconImageViewWidgets) ~
+                        (sampleColor4Icon <~ pmdAnimIcon(TypeIcons.CHECK))
+                  }
+                ) <~ tableLayoutRowStyle
+              ) <~ tableLayoutStyle <~ wire(colorSelectorGroup),
+              w[TextView] <~ wire(strokeTitle) <~ tvText(R.string.title_select_stroke) <~ titleStyle,
+              w[SeekBar] <~ wire(strokeSelector) <~ strokeStyle <~ sbOnSeekBarChangeListener(
+                OnSeekBarChangeListenerHandler(
+                  onProgressChangedHandler = (view: SeekBar, progress: Int, fromUser: Boolean) => {
+                    val stroke = progress + 1
+
+                    (strokeTitle <~ tvText(context.get.getResources().getString(R.string.title_select_stroke, stroke.toString))) ~
+                        (icon <~ pmdStroke(stroke dp))
+                  }
+                )
+              ),
+              w[TextView] <~ wire(sizeTitle) <~ tvText(R.string.title_select_size) <~ titleStyle,
+              w[SeekBar] <~ wire(sizeSelector) <~ sizeStyle <~ sbOnSeekBarChangeListener(
+                OnSeekBarChangeListenerHandler(
+                  onStopTrackingTouchHandler = (view: SeekBar) => {
+                    val Dim(width, height) = sizeOptionList(view.getProgress())
+
+                    (sizeTitle <~ tvText(context.get.getResources().getString(R.string.title_select_size, width.toString, height.toString))) ~
+                        (colorSelectorGroup <~ setNoIconImageViewWidgets) ~
+                        (sampleColor1Icon <~ pmdAnimIcon(TypeIcons.CHECK)) ~
+                        (strokeSelector <~ sbProgress(2)) ~
+                        (icon <~ drawableStyle(width dp, height dp, 3 dp) <~ pmdColorResource(R.color.pathMorphSample1)) ~
+                        (iconSelectorGroup <~ deactivateImageViewWidgets)
+                  }
+                )
+              )
+            ) <~ verticalLinearLayoutStyle
+          ) <~ scrollViewStyle
         ) <~ contentStyle
       ) <~ rootStyle
     )
