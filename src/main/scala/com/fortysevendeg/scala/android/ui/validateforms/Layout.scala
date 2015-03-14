73c27ec0730d378fd0c2d2a1728417814c619da6
Juan Pedro Moreno <juanpmorest@gmail.com>
2015-03-14 15:34:58 +0100

    SA-19 added labels and event in order to validate the fields

--- src/main/scala/com/fortysevendeg/scala/android/ui/validateforms/Layout.scala
+++ src/main/scala/com/fortysevendeg/scala/android/ui/validateforms/Layout.scala
@@ -1,6 +1,7 @@
 package com.fortysevendeg.scala.android.ui.validateforms
 
 import android.widget._
+import com.fortysevendeg.macroid.extras.TextTweaks._
 import com.fortysevendeg.macroid.extras.ToolbarTweaks._
 import com.fortysevendeg.scala.android.R
 import com.fortysevendeg.scala.android.ui.commons.ToolbarLayout
@@ -8,14 +9,16 @@ import macroid.FullDsl._
 import macroid.{ActivityContext, AppContext}
 
 trait Layout
-  extends ToolbarLayout
-  with Styles {
+    extends ToolbarLayout
+    with Styles {
 
   var name = slot[EditText]
 
   var age = slot[EditText]
 
   var email = slot[EditText]
+
+  var validationResultLabel = slot[TextView]
 
   def layout(implicit appContext: AppContext, context: ActivityContext) = {
     getUi(
@@ -28,11 +31,13 @@ trait Layout
             w[TextView] <~ textLabelStyle <~ text(R.string.validate_label_email),
             w[EditText] <~ editTextStyle <~ wire(email),
             w[TextView] <~ textLabelStyle <~ text(R.string.validate_label_age),
-            w[EditText] <~ editTextStyle <~ wire(age)
+            w[EditText] <~ editTextStyle <~ wire(age),
+            w[TextView] <~ textLabelStyle <~ text(R.string.validate_label_result),
+            w[TextView] <~ textLabelStyle <~ text(R.string.validate_result) <~ wire(validationResultLabel)
           ) <~ scrollContentStyle
         ) <~ scrollStyle,
         w[ImageView] <~ lineHorizontalStyle,
-        w[Button] <~ saveButtonStyle
+        w[Button] <~ saveButtonStyle <~ On.click(validationResultLabel <~ tvText("Hi"))
       ) <~ contentStyle
     )
   }
