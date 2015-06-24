660534a489c4cbdf70c9c42e25cc1f82d35a3b78
Fede Fernández <federico.f.b@47deg.com>
2015-06-24 11:08:04 +0200

    Splits commands one per line

--- scripts/runPackage.sh
+++ scripts/runPackage.sh
@@ -1,3 +1,5 @@
 #!/bin/bash 
 set -ev
-sbt ";clean; android:package; android:package"
+sbt ++$TRAVIS_SCALA_VERSION clean
+sbt ++$TRAVIS_SCALA_VERSION android:package
+sbt ++$TRAVIS_SCALA_VERSION android:package
